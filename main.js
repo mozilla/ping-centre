04c77e2d7c3661f92421252b500923c63db360c5
Marina Samuel <msamuel@mozilla.com>
2016-11-23 17:13:30 -0500

    Add HTTP POST request.

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
 "use strict";
 
+require('isomorphic-fetch');
+const commonSchema = require('./schemas/commonSchema');
+const Joi = require('joi');
+const uuid = require('uuid');
+
+const PING_ENDPOINT = "";
+
 class PingCentre {
-  constructor(apiKey, clientID) {
-    if (!apiKey) {
-      throw new Error("Must specify API key.");
+  constructor(topic, clientID, pingEndpoint, schema) {
+    if (!topic) {
+      throw new Error("Must specify topic.");
     }
-    this._clientID = clientID;
-    this._apiKey = apiKey;
+    this._topic = topic
+    this._clientID = clientID || uuid();
+    this._schema = schema || commonSchema;
+    this._pingEndpoint = pingEndpoint || PING_ENDPOINT;
+    this._resetDataObject();
+  }
+
+  _resetDataObject() {
+    this._data = {
+      topic: this._topic,
+      client_id: this._clientID
+    };
   }
 
   sendPing(data) {
-    if (typeof data !== 'object') {
-      throw new Error(`Expecting object got ${typeof data}`);
-    }
-    if (!data || Object.keys(data).length === 0) {
-      throw new Error(`Empty data object`);
-    }
+    let payload = Object.assign(this._data, data);
+    this._resetDataObject();
 
-    console.log("Sending ping to Onyx");
+    Joi.validate(payload, this._schema, function (err, value) {
+      if (err) {
+        throw new Error(`Invalid object schema ${JSON.stringify(value)}: ${err}.`);
+      }
+    });
 
-    // TODO: Send ping to onyx.
-    // fetch(this._pingEndpoint, {method: "POST", body: data})
+    return fetch(this._pingEndpoint, {method: "POST", body: payload}).then(response => {
+      if (!response.ok) {
+        console.error(`Ping failure with response code: ${response.status}`);
+      }
+
+    }).catch(e => {
+      console.error(`Ping failure with error code: ${e.message}`);
+    });
   }
 }
 module.exports = PingCentre;
