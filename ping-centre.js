055d714bae8a4fe20ad1362da04904c77f537f09
Marina Samuel <msamuel@mozilla.com>
2016-11-25 12:01:59 -0500

    Closes #11: Wait for schema validation before POST.

--- ping-centre.js
+++ ping-centre.js
@@ -18,25 +18,35 @@ class PingCentre {
     this._pingEndpoint = pingEndpoint || PING_ENDPOINT;
   }
 
+  validate(payload) {
+    return new Promise((resolve, reject) => {
+      Joi.validate(payload, this._schema, (err, value) => {
+        if (err) {
+          reject(new Error(`Invalid payload ${JSON.stringify(value)}: ${err}.`));
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
   sendPing(data) {
     let payload = Object.assign({
       topic: this._topic,
       client_id: this._clientID
     }, data);
 
-    Joi.validate(payload, this._schema, function (err, value) {
-      if (err) {
-        throw new Error(`Invalid object schema ${JSON.stringify(value)}: ${err}.`);
-      }
-    });
-
-    return fetch(this._pingEndpoint, {method: "POST", body: payload}).then(response => {
-      if (!response.ok) {
-        console.error(`Ping failure with response code: ${response.status}`);
-      }
-
+    return this.validate(payload).then(() => {
+      return fetch(this._pingEndpoint, {method: "POST", body: payload}).then(response => {
+        if (!response.ok) {
+          throw new Error(`Ping failure with response code: ${response.status}`);
+        } else {
+          return response;
+        }
+      })
     }).catch(e => {
       console.error(`Ping failure with error code: ${e.message}`);
+      throw e;
     });
   }
 }
