db41bb89286c8b79139a2dead8ca9985248c6158
Marina Samuel <msamuel@mozilla.com>
2016-11-25 12:26:49 -0500

    Closes #12: Test case for request failure.

--- test/ping-centre.js
+++ test/ping-centre.js
@@ -6,6 +6,14 @@ let PingCentre = require('../ping-centre');
 const topic = "testpilot_server";
 let pingClient = new PingCentre(topic);
 
+before(() => {
+  fetchMock.mock('*', {});
+});
+
+after(() => {
+  fetchMock.restore();
+});
+
 describe('Ping Centre Throws', function() {
   it('throws if topic is empty', function() {
     assert.throws(() => new PingCentre("", "clientID"));
@@ -14,23 +22,19 @@ describe('Ping Centre Throws', function() {
 
 describe('Joi Handles Various Cases', function() {
   it("rejects undefined data", function(done) {
-    fetchMock.mock('*', {});
     let promise = pingClient.sendPing();
     promise.should.be.rejected.notify(done);
   });
   it("rejects if data is not an object", function(done) {
-    fetchMock.mock('*', {});
     pingClient.sendPing(45).should.be.rejected.notify(done);
   });
   it("rejects if data is an empty object", function(done) {
-    fetchMock.mock('*', {});
     pingClient.sendPing({}).should.be.rejected.notify(done);
   });
 });
 
 describe('Ping Centre Common Properties', function() {
   it('has the expected common properties', function(done) {
-    fetchMock.mock('*', {});
 
     let event_type = "testpilot.test-install"
     pingClient.sendPing({
@@ -40,17 +44,25 @@ describe('Ping Centre Common Properties', function() {
       assert.isNotNull(fetchMock.lastOptions("*").body.client_id, "client_id exists in payload");
       assert.equal(fetchMock.lastOptions("*").body.event_type, event_type, "event_type exists in payload");
 
-      fetchMock.restore();
       done();
     });
   });
   it('does not throw when additional fields exist', function(done) {
-    fetchMock.mock('*', {});
     pingClient = new PingCentre(topic, null, "http://www.test.com");
     pingClient.sendPing({
       event_type: "test",
       additional_field: "shouldn't throw"
     }).should.be.fulfilled.notify(done);
+  });
+});
+
+describe('Ping Centre Handles Server Errors', function() {
+  it("handles 400 error", function(done) {
     fetchMock.restore();
+    fetchMock.mock('*', 400);
+    pingClient.sendPing({
+      event_type: "test",
+      additional_field: "should reject"
+    }).should.be.rejected.notify(done);
   });
 });
