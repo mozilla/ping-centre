82e6239732cd0bff12ff8942bcf24d8254d6d803
Marina Samuel <msamuel@mozilla.com>
2016-11-25 13:41:35 -0500

    Closes #9: Add eslint and lint existing files.

--- schemas/commonSchema.js
+++ schemas/commonSchema.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const Joi = require('joi');
+const Joi = require("joi");
 
-let schema = Joi.object().keys({
+const schema = Joi.object().keys({
   client_id: Joi.string().required(),
   topic: Joi.string().required(),
   event_type: Joi.string().required(),
-}).options({ allowUnknown: true });
+}).options({allowUnknown: true});
 
 module.exports = schema;
