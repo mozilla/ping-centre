e0b86f37cf2c6371b8b3c781e2562ec5019aeaa5
Marina Samuel <msamuel@mozilla.com>
2017-01-05 18:12:27 -0500

    Closes #14: Move endpoints to config files.

---
+++ test/endpoints.js
@@ -0,0 +1,25 @@
+/* global assert */
+"use strict";
+
+const topic = "sample";
+
+describe("Ping Centre Endpoints", function() {
+  it("default endpoint is used by default", function() {
+    const PingCentre = require("../ping-centre");
+    const pingClient = new PingCentre(topic);
+    assert.equal(pingClient._pingEndpoint, "https://onyx_tiles.stage.mozaws.net/v3/links/ping-centre");
+  });
+
+  it("prod endpoint is used when prod environment variable is set", function() {
+    // Clear cache for ping-centre and config since ping-centre needs to be
+    // required again in order to require config again and look up the
+    // newly set environment variables.
+    delete require.cache[require.resolve("../ping-centre")];
+    delete require.cache[require.resolve("config")];
+
+    process.env["NODE_ENV"] = "production";
+    const PingCentre = require("../ping-centre");
+    const pingClient = new PingCentre(topic);
+    assert.equal(pingClient._pingEndpoint, "https://tiles.services.mozilla.com/v3/links/ping-centre");
+  });
+});
