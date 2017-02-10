f2a39de9be6b134695a0c7820e2e9052c74878c8
Marina Samuel <msamuel@mozilla.com>
2017-02-10 14:59:11 -0500

    Adding babel so minifying works.

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,12 @@ module.exports = {
     // name of the global variable
     library: "PingCentre"
   },
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {presets: ["es2015"]}
+    }]
+  }
 };
